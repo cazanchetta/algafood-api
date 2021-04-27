96405bcdee68a5582e056d0f21a98e7e2af5139d
Carlos Eduardo Zanchetta <cazanchetta@gmail.com>
2021-04-27 19:33:03 -0300

    inicializando repositorio

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.di.notificacao;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+
+public class NotificadorEmail implements Notificador {
+	
+	private boolean caixaAlta;
+	private String hostServidorSmtp;
+	
+	public NotificadorEmail(String hostServidorSmtp) {
+		this.hostServidorSmtp = hostServidorSmtp;
+		System.out.println("NotificadorEmail");
+	}
+	
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		if(caixaAlta) {
+			mensagem = mensagem.toUpperCase();
+		}
+		
+		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+	}
+	
+	public void setCaixaAlta(boolean caixaAlta) {
+		this.caixaAlta = caixaAlta;
+	}
+	
+}
